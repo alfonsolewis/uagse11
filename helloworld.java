f1f6b934ed6c0600076e1a1e6b5ccf3455694822
alfonsolewis <alfonsolewis@uagateway.org>
2016-10-24 13:07:48 +0000

    initial commit

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I am going to do so much work this summer!");
+    }
+}
